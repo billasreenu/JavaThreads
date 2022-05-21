83f3e60df496502cae16cae1594f2c320904e88e
Sreenu Billa <sreenivasaraobilla@gmail.com>
2022-05-21 14:28:45 +0530

    code changes

--- src/main/java/com/billa/javathreads/ProductCallable.java
+++ src/main/java/com/billa/javathreads/ProductCallable.java
@@ -1,6 +1,9 @@
 package com.billa.javathreads;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
 
 import com.billa.javathreads.util.SysOutLogger;
@@ -16,7 +19,7 @@ public class ProductCallable implements Callable<ExcelRecordVO> {
 		return vo;
 	}
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		SysOutLogger.info("[ProductCallable] (main) START");
 
 		ProductCallable c = new ProductCallable();
@@ -40,4 +43,26 @@ public class ProductCallable implements Callable<ExcelRecordVO> {
 
 		SysOutLogger.info("[ProductCallable] (main) END");
 	}
+
+	public static void main(String[] args) {
+		SysOutLogger.info("[ProductCallable] (main) START");
+
+		ProductCallable th = new ProductCallable();
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		Future futureTask = executor.submit(th);
+		try {
+			while (!futureTask.isDone()) {
+				SysOutLogger.info("[ProductCallable] (main) wait for 5 sec");
+				Thread.sleep(5_000l);
+			}
+			ExcelRecordVO vo = (ExcelRecordVO) futureTask.get();
+			SysOutLogger.info(vo.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			executor.shutdown();
+		}
+		SysOutLogger.info("[ProductCallable] (main) END");
+	}
+
 }
--- src/main/java/com/billa/javathreads/ProductRunnable.java
+++ src/main/java/com/billa/javathreads/ProductRunnable.java
@@ -1,17 +1,32 @@
 package com.billa.javathreads;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.billa.javathreads.util.SysOutLogger;
+
 public class ProductRunnable implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println("[ProductRunnable] (run)  Thread name -" + Thread.currentThread().getName());
+		SysOutLogger.info("[ProductRunnable] (run) from runnable thread");
+	}
+
+	public static void main1(String[] args) {
+		ProductRunnable th = new ProductRunnable();
+		Thread t = new Thread(th);
+		System.out.println("[ProductRunnable] (main)");
+		t.start();
+		System.out.println("[ProductRunnable] (main)");
 	}
 
 	public static void main(String[] args) {
+		SysOutLogger.info("[ProductRunnable] (main) START");
+
 		ProductRunnable th = new ProductRunnable();
-		Thread t = new Thread(th);
-		System.out.println("[ProductRunnable] (main) Thread name -" + Thread.currentThread().getName());
-		t.start();
-		System.out.println("[ProductRunnable] (main) Thread name -" + Thread.currentThread().getName());
+		ExecutorService executor = Executors.newSingleThreadExecutor();
+		executor.execute(th);
+
+		SysOutLogger.info("[ProductRunnable] (main) END");
 	}
 }
