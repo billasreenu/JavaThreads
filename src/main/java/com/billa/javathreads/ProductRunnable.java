26c0d49daa268277c4c36eaf9692fc470f78ff0d
Sreenu Billa <sreenivasaraobilla@gmail.com>
2022-05-21 13:48:06 +0530

    code changes

--- src/main/java/com/billa/javathreads/ProductRunnable.java
+++ src/main/java/com/billa/javathreads/ProductRunnable.java
@@ -4,7 +4,14 @@ public class ProductRunnable implements Runnable {
 
 	@Override
 	public void run() {
-		System.out.println("[ProductRunnable] (run) hello");
+		System.out.println("[ProductRunnable] (run)  Thread name -" + Thread.currentThread().getName());
 	}
 
+	public static void main(String[] args) {
+		ProductRunnable th = new ProductRunnable();
+		Thread t = new Thread(th);
+		System.out.println("[ProductRunnable] (main) Thread name -" + Thread.currentThread().getName());
+		t.start();
+		System.out.println("[ProductRunnable] (main) Thread name -" + Thread.currentThread().getName());
+	}
 }
--- src/main/java/com/billa/javathreads/ProductThread.java
+++ src/main/java/com/billa/javathreads/ProductThread.java
@@ -1,9 +1,16 @@
 package com.billa.javathreads;
 
 public class ProductThread extends Thread {
+
 	@Override
 	public void run() {
-		System.out.println("[ProductThread] (run) hello");
+		System.out.println("[ProductThread] (run) Thread name -" + Thread.currentThread().getName());
 	}
 
+	public static void main(String[] args) {
+		ProductThread th = new ProductThread();
+		System.out.println("[ProductThread] (main) Thread name -" + Thread.currentThread().getName());
+		th.start();
+		System.out.println("[ProductThread] (main) Thread name -" + Thread.currentThread().getName());
+	}
 }
